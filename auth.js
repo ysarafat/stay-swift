202b8f43648ad3d903c0316c65ec2e21b5451ac5
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-08-11 18:31:36 +0600

    save user to database

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import mongoClientPromise from "./database/mongoClientPromise";
 
 export const {
   auth,
@@ -7,6 +9,9 @@ export const {
   signOut,
   handlers: { GET, POST },
 } = NextAuth({
+  adapter: MongoDBAdapter(mongoClientPromise, {
+    databaseName: process.env.ENVIRONMENT,
+  }),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -2,15 +2,27 @@
 
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
-const SocialLogins = () => {
+const SocialLogins = ({ mode }) => {
   function handleAuth() {
     signIn("google", { callbackUrl: "http://localhost:3000/bookings" });
   }
 
   return (
     <>
-      <div className="text-center text-xs text-gray-500">or Signup with</div>
+      <div className="text-center text-xs text-gray-500">
+        {mode === "register" ? (
+          <Link className="underline" href={"/login"}>
+            Login
+          </Link>
+        ) : (
+          <Link className="underline" href={"/register"}>
+            Register
+          </Link>
+        )}{" "}
+        or Signup with
+      </div>
       <div className="flex gap-4">
         <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
