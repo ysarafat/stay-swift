0de8a98251bb04c665730009f60fb7c136df167c
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-08-11 22:38:22 +0600

    implement credentials login

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import mongoClientPromise from "./database/mongoClientPromise";
+import { userModel } from "./model/user-model";
 
 export const {
   auth,
@@ -12,7 +14,36 @@ export const {
   adapter: MongoDBAdapter(mongoClientPromise, {
     databaseName: process.env.ENVIRONMENT,
   }),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
+    CredentialProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (credentials === null) {
+          return null;
+        }
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          if (user) {
+            const isMatch = user.email === credentials.email;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Invalid credentials");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- components/auth/LoginForm.jsx
+++ components/auth/LoginForm.jsx
@@ -1,20 +1,50 @@
+"use client";
+
+import { login } from "@/actions";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const LoginForm = () => {
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    setError(null);
+    try {
+      const formData = new FormData(e.currentTarget);
+      const res = await login(formData);
+      console.log(res);
+      if (!!res.error) {
+        setError(res.error.message);
+      } else {
+        setError(null);
+        router.push("/bookings");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
   return (
-    <form className="login-form">
-      <div>
-        <label htmlFor="email">Email Address</label>
-        <input type="email" name="email" id="email" />
-      </div>
+    <>
+      {error && <p className="text-red-500">{error}</p>}
+      <form className="login-form" onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" id="email" />
+        </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
 
-      <button type="submit" className="btn-primary w-full mt-4">
-        Login
-      </button>
-    </form>
+        <button type="submit" className="btn-primary w-full mt-4">
+          Login
+        </button>
+      </form>
+    </>
   );
 };
 
