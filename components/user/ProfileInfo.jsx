64ada4f2e29b44d24f145e2f3b7497e6cb477292
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-08-12 15:28:46 +0600

    show authenticate user details in profile page

--- components/user/ProfileInfo.jsx
+++ components/user/ProfileInfo.jsx
@@ -1,18 +1,32 @@
-const ProfileInfo = () => {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function ProfileInfo() {
+  const session = await auth();
   return (
     <div className="flex flex-col items-center py-8 text-center">
       <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-        S
+        {session?.user?.image ? (
+          <Image
+            src={session?.user?.image}
+            alt="user"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        ) : (
+          session?.user?.name.charAt(0).toUpperCase()
+        )}
       </div>
 
       <div>
-        <h3 className="text-2xl font-semibold lg:text-[28px]">Sumit Saha</h3>
-        <p className="leading-[231%] lg:text-lg">sumitsaha@gmail.com</p>
+        <h3 className="text-2xl font-semibold lg:text-[28px]">
+          {session?.user?.name}
+        </h3>
+        <p className="leading-[231%] lg:text-lg">{session?.user?.email}</p>
       </div>
 
       <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4"></div>
     </div>
   );
-};
-
-export default ProfileInfo;
+}
